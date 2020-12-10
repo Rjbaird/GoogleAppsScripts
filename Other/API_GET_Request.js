a32d3fe480486aece166238def1830428c5bb6eb
Ryan Baird <ryanjohnbaird@gmail.com>
2020-12-09 22:28:22 -0600

    added API call snippet

---
+++ Other/API_GET_Request.js
@@ -0,0 +1,25 @@
+/*
+// =============== Snippet Overview =============== //
+This snippet allows you to make an API Get Request for api following the REST protocol.
+Simply update the token and baseUrl variables for the api that you are using.
+Call the function as a variable and then pass that variable to another function that parses the data
+
+*/
+
+function apiGetRequest() {
+    var token = 'ENTER TOKEN HERE'; //enter your API token inside the single quotation marks if required
+    var baseUrl = 'ENTER BASE URL HERE'; // enter the base url for the API call here
+    var headers = { 'Authorization': 'Bearer ' + token };
+    var params = {
+        'method': 'GET',
+        'headers': headers,
+        'contentType': 'application/json'
+    };
+    var responseData = JSON.parse(UrlFetchApp.fetch(baseUrl, params))
+    return responseData
+}
+
+function makeApiCallAndParseData() {
+    var data = apiGetRequest();
+    parseData(data)
+}
